319672a1374cdc15e8098a8d5b9b29c7badddc01
Ruth Mwangi <rmwangi22020@gmail.com>
2023-02-08 23:44:02 +0300

    ejs connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 require('dotenv').config();
 
 // express app
@@ -8,15 +9,26 @@ const app = express();
 const PORT = process.env.PORT;
 
 
+
 // connect to mongodb
 const connectionString = process.env.DATABASE_URL;
 mongoose.connect(connectionString);
 // // to prevent duplication
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 // route export
 app.use('/api',require('./routes/api'))
+
+//register view engine
+app.set('view engine', 'ejs');
+//middleware and static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+
+
+
 
 // listen for requests
 app.listen(PORT, () => {
@@ -24,8 +36,26 @@ app.listen(PORT, () => {
  });
 
 
-
-
-
-
-
+//  / view engine connection
+ app.get('/', (req, res)=>{
+    res.render('index');
+    
+    });
+   //  404 error
+  
+app.use((req, res) =>{
+   res.status(404).render('404', {title: '404'});
+   router.patch('/:id', async function (req, res) {
+     const id = req.params.id;
+     const updatedData = req.body;
+     console.log(id)
+     console.log(updatedData)
+  
+     try {
+         const dataUpdate = await bookingModel.findByIdAndUpdate(id, updatedData, { new: true });
+         res.status(200).json(dataUpdate);
+     } catch (error) {
+         res.status(400).json({ message: error.message });
+     }
+  });      
+  });
